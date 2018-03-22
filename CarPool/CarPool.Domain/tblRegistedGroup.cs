fa3fc788e77c590174ce98726bd576866383d0c6
jigarkhalasi <jigs.prince78@gmail.com>
2018-03-22 23:33:33 +0530

    Register Api done

---
+++ CarPool/CarPool.Domain/tblRegistedGroup.cs
@@ -0,0 +1,58 @@
+namespace CarPool.Domain
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("tblRegistedGroup")]
+    public partial class tblRegistedGroup
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int RGroupId { get; set; }
+
+        public int? RGType { get; set; }
+
+        [StringLength(1000)]
+        public string RGName { get; set; }
+
+        [StringLength(50)]
+        public string StartFrom { get; set; }
+
+        [StringLength(50)]
+        public string EndFrom { get; set; }
+
+        public string RGDesc { get; set; }
+
+        public DateTime? RGCreatedDate { get; set; }
+
+        public byte[] RGImagePath { get; set; }
+
+        public bool? IsAutoRegister { get; set; }
+
+        [StringLength(50)]
+        public string RGLink { get; set; }
+
+        public int? IsRanking { get; set; }
+
+        public int? City { get; set; }
+
+        public int? Country { get; set; }
+
+        public bool? IsActivate { get; set; }
+
+        public bool? IsDeleted { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+
+        [StringLength(50)]
+        public string CreatedBy { get; set; }
+
+        public DateTime? UpdatedDate { get; set; }
+
+        [StringLength(50)]
+        public string UpdatedBy { get; set; }
+    }
+}
---
+++ CarPool/CarPool/Startup.cs
@@ -0,0 +1,51 @@
+﻿using CarPool.Helper;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+[assembly: OwinStartup(typeof(CarPool.Startup))]
+namespace CarPool
+{
+    public class Startup
+    {
+        public static OAuthBearerAuthenticationOptions OAuthBearerOptions { get; private set; }
+
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            ConfigureOAuth(app);
+
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            AreaRegistration.RegisterAllAreas();
+
+            WebApiConfig.Register(config);
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            app.UseWebApi(config);
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new SimpleAuthorizationServerProvider()
+            };
+
+            // Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+        }
+    }
+}
